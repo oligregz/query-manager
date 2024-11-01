1fda76687a231438ebc64f1484816a37cffce09e
oligregz <gregorysak90@gmail.com>
2024-11-01 10:19:02 -0300

    fix: change set date value

--- src/utils/formatDateStringForISO8601.ts
+++ src/utils/formatDateStringForISO8601.ts
@@ -1,9 +1,6 @@
 export const formatDateStringForISO8601 = (dateString: string): string => {
   const date = new Date(dateString)
 
-  date.setDate(date.getDate() + 2)
-  date.setMinutes(date.getMinutes() + 30)
-
   const year = date.getFullYear()
   const month = String(date.getMonth() + 1).padStart(2, '0')
   const day = String(date.getDate()).padStart(2, '0')
